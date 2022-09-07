98ed134d639ec41cb4b5526b94771ac075bf3d2b
batnapquantaitimnguoitriky <112962883+batnapquantaitimnguoitriky@users.noreply.github.com>
2022-09-07 09:14:30 +0700

    vanduyentong

---
+++ vanduyentong/baitap.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "..\_COMMON\Log.h"
+
+int main(int argc, char const *argv[])
+{
+  LOG_D("WELCOME C++. TVDUYEN\n");
+  LOG_I("WELCOME C++. TVDUYEN\n");
+  LOG_W("WELCOME C++. TVDUYEN\n");
+  LOG_E("WELCOME C++. TVDUYEN\n");
+  LOG_D("");
+  return 0;
+}
