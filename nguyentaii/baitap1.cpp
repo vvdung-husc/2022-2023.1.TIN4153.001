fce15847066bcb0fc6c9518bc351c0483f07fd14
nguyentai0606 <112935665+nguyentai0606@users.noreply.github.com>
2022-09-07 09:11:20 +0700

    nguyentai0606

--- nguyentaii/baitap1.cpp
+++ nguyentaii/baitap1.cpp
@@ -1,5 +1,18 @@
 #include <stdio.h>
-int main(int argc, const char** argv){
-    printf("Welcome C++\n");
-    return 0;
+#include "..\_COMMON\Log.h"
+
+int main(int argc, char const *argv[])
+{
+  LOG_D("WELCOME C++. NTai\n");
+  LOG_I("WELCOME C++. NTai\n");
+  LOG_W("WELCOME C++. NTai\n");
+  LOG_E("WELCOME C++. NTai\n");
+
+  LOG_DT("WELCOME C++. NTai\n");
+  LOG_IT("WELCOME C++. NTai\n");
+  LOG_WT("WELCOME C++. NTai\n");
+  LOG_ET("WELCOME C++. NTai\n");
+
+  LOG_D("");
+  return 0;
 }
