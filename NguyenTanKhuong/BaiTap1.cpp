509dd7647bc1797991e91571e8855e4fa8ca4e7c
Tan Khuong Nguyen <96215403+AndrewTanKhuong@users.noreply.github.com>
2022-09-07 08:54:52 +0700

    AndrewTanKhuong

--- NguyenTanKhuong/BaiTap1.cpp
+++ NguyenTanKhuong/BaiTap1.cpp
@@ -1,5 +1,19 @@
 #include <stdio.h>
-int main(int argc, const char** argv){
-    printf("Welcome C++\n");
-    return 0;
+#include "..\_COMMON\Log.h"
+
+int main(int argc, char const *argv[])
+{
+  LOG_D("WELCOME C++. NTKhuong\n");
+  LOG_I("WELCOME C++. NTKhuong\n");
+  LOG_W("WELCOME C++. NTKhuong\n");
+  LOG_E("WELCOME C++. NTKhuong\n");
+  LOG_D("");
+
+  LOG_DT("WELCOME C++. NTKhuong\n");
+  LOG_IT("WELCOME C++. NTKhuong\n");
+  LOG_WT("WELCOME C++. NTKhuong\n");
+  LOG_ET("WELCOME C++. NTKhuong\n");
+  LOG_DT("");
+
+  return 0;
 }
