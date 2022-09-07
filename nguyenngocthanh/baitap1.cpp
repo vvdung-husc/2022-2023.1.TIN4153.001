5c7353b5e751e3480f95278f2bbca550ea6e06f7
Nguyenngocthanh2708 <19t1101002@husc.edu.vn>
2022-09-07 10:10:22 +0700

    baitap

--- nguyenngocthanh/baitap1.cpp
+++ nguyenngocthanh/baitap1.cpp
@@ -7,6 +7,11 @@ int main(int argc, char const *argv[])
   LOG_I("WELCOME C++. VVDUNG\n");
   LOG_W("WELCOME C++. VVDUNG\n");
   LOG_E("WELCOME C++. VVDUNG\n");
+
+  LOG_DT("WELCOME C++. VVDUNG\n");
+  LOG_IT("WELCOME C++. VVDUNG\n");
+  LOG_WT("WELCOME C++. VVDUNG\n");
+  LOG_ET("WELCOME C++. VVDUNG");
   LOG_D("");
   return 0;
 }
