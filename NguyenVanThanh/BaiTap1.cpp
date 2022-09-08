1e7deaba2ceadddf27d468131c6a073461a262c2
NguyenThanh2001 <nguyenthanh280201@gmail.com>
2022-09-08 17:55:36 +0700

    NGUYENTHANH270101

---
+++ NguyenVanThanh/BaiTap1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "..\_COMMON\Log.h"
+
+int main(int argc, char const *argv[])
+{
+  LOG_D("WELCOME C++. NVThanh\n");
+  LOG_I("WELCOME C++. NVThanh\n");
+  LOG_W("WELCOME C++. NVThanh\n");
+  LOG_E("WELCOME C++. NVThanh\n");
+  LOG_D("");
+
+  LOG_DT("WELCOME C++. NVThanh\n");
+  LOG_IT("WELCOME C++. NVThanh\n");
+  LOG_WT("WELCOME C++. NVThanh\n");
+  LOG_ET("WELCOME C++. NVThanh\n");
+  LOG_DT("");
+
+  return 0;
+}
