aab4ff491a27b70ef28bbfb47c9731a944341eb2
huyle55 <ledinhngochuy55@gmail.com>
2022-09-08 17:43:15 +0700

    Huyle12

---
+++ LeDinhNgocHuy/BaiTap1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "..\_COMMON\Log.h"
+
+int main(int argc, char const *argv[])
+{
+  LOG_D("WELCOME C++. LDNHuy\n");
+  LOG_I("WELCOME C++. LDNHuy\n");
+  LOG_W("WELCOME C++. LDNHuy\n");
+  LOG_E("WELCOME C++. LDNHuy\n");
+  LOG_D("");
+
+  LOG_DT("WELCOME C++. LDNHuy\n");
+  LOG_IT("WELCOME C++. LDNHuy\n");
+  LOG_WT("WELCOME C++. LDNHuy\n");
+  LOG_ET("WELCOME C++. LDNHuy\n");
+  LOG_DT("");
+
+  return 0;
+}
