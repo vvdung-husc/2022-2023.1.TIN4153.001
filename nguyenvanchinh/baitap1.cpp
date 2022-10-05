89ebe68feb5630820f0ee9998db560aa46fbd181
chinh <19t1021023@husc.edu.vn>
2022-10-05 07:38:42 +0700

    19t1021023

---
+++ nguyenvanchinh/baitap1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "..\_COMMON\Log.h"
+
+int main(int argc, char const *argv[])
+{
+  LOG_D("WELCOME C++. NVChinh\n");
+  LOG_I("WELCOME C++. NVChinh\n");
+  LOG_W("WELCOME C++. NVChinh\n");
+  LOG_E("WELCOME C++. NVChinh\n");
+  LOG_D("");
+
+  LOG_DT("WELCOME C++. NVChinh\n");
+  LOG_IT("WELCOME C++. NVChinh\n");
+  LOG_WT("WELCOME C++. NVChinh\n");
+  LOG_ET("WELCOME C++. NVChinh\n");
+  LOG_DT("");
+
+  return 0;
+}
